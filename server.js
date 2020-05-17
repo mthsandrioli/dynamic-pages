ef43b651d146c85991f6be581ec7594160536586
Matheus Andrioli <matheus_andrioli@hotmail.com>
2020-05-17 03:09:49 -0300

    feature: Adiciona nova rota 'video'

--- server.js
+++ server.js
@@ -43,6 +43,22 @@ server.get('/topics', function(req, res) {
   return res.render('topics', { items: videos})
 })
 
+server.get('/video', function(req, res) {
+  const id = req.query.id
+
+  const video = videos.find(function(video) {
+    if (video.id == id) {
+      return true;
+    }
+  })
+
+  if (!video) {
+    return res.send('Video not found!')
+  }
+
+  return res.render('video', { item: video })
+})
+
 server.listen(5000, function() {
   console.log('server is running')
 })
